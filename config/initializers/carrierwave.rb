5cce781f9662c514e7da38849e3f7857fbeabcfa
Tu Dat Nguyen <nguyentudat@gmail.com>
2018-04-30 12:35:16 -0700

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:                         'Google',
-    google_storage_access_key_id:     'GOOGYWB53C7IH2RNJ7GG',
-    google_storage_secret_access_key: 'kIRXrpOs+XeoNCKAqMbT75xmVE1SLAOrVTHfI5t4'
+    google_storage_access_key_id:     '',
+    google_storage_secret_access_key: ''
   }
   config.fog_directory = 'geobox1'
 end
